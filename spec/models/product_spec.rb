b281707596cef6e011a3f6bdd4c006d34375d1ba
mantanquzzmo <quzzmo@hotmail.com>
2019-12-10 16:47:24 +0100

    Fixes intendation and removes unneccesary line of code

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,9 +13,7 @@ RSpec.describe Product, type: :model do
     it { is_expected.to validate_presence_of :price } 
   end
 
-  describe 'Factory' do
-    it 'should have valid Factory' do
+  it 'should have valid Factory' do
       expect(FactoryBot.create(:product)).to be_valid 
-    end
   end
 end
