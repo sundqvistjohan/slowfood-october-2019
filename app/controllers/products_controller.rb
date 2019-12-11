c549d5e37bf4c4b530b246e94f127ab8c74aafed
Eevan <eevan.redon@gmail.com>
2019-12-11 13:47:24 +0100

    work in progress

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
   def index
-    @
+    @categories = Category.all
   end
 end
--- features/step_definitions/background_step.rb
+++ features/step_definitions/background_step.rb
@@ -1,5 +1,7 @@
 Given("the following products exist:") do |table|
   table.hashes.each do |product_attributes|
-    create(:product, product_attributes)
+    category = Category.find_or_create_by(name: product_attributes["category"])
+    product_attributes.except!("category")
+    create(:product, product_attributes.merge(category: category))
   end
 end
