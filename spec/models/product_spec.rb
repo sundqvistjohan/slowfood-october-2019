fca5447e08ae1ba46574aada9e305bbfc256946d
Johan Sundqvist <sundqvist.johan@outlook.com>
2019-12-12 11:25:59 +0100

    adds association test for product to category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,6 +13,10 @@ RSpec.describe Product, type: :model do
     it { is_expected.to validate_presence_of :price } 
   end
 
+  describe 'Associations' do
+    it { is_expected.to belong_to :category }
+  end
+
   describe 'Factory' do
     it 'should have valid Factory' do
       expect(create(:product)).to be_valid
