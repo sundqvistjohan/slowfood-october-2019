24facbd9a741bd41c39b88c1cd9eb1a8265aabdc
Johan Sundqvist <sundqvist.johan@outlook.com>
2019-12-14 19:27:24 +0100

    adds unit tests for owner

--- spec/factories/owners.rb
+++ spec/factories/owners.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :owner do
-    
+    email { "MyString@mail.com" }
+    password { "MyString" }
   end
 end
--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,5 +1,14 @@
-require 'rails_helper'
+RSpec.describe User, type: :model do
+  describe 'DB table' do
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :encrypted_password }
+  end
 
-RSpec.describe Owner, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many :orders }
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(create(:owner)).to be_valid 
+    end
+  end
 end
